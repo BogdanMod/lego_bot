90aa59bd5d3d50a269078bd8ca97d073abda4916
BogdanMod <bogdik2211@gmail.com>
2026-01-19 02:50:07 +0300

    fix: исправлен импорт в api/index.ts для работы с dist

--- packages/core/api/index.ts
+++ packages/core/api/index.ts
@@ -1,6 +1,8 @@
 // Vercel Serverless Function
 // Экспортируем Express app для работы в Vercel
-import app from '../src/index';
+// После build команды, dist/index.js будет доступен
+// @ts-ignore - dist файлы могут не иметь типов
+import app from '../dist/index';
 
 export default app;
 
